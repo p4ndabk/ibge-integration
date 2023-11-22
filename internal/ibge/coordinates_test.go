43fed6d33def93e92e609766aa02d082064af27a
davidsilva <david.silva@aiqfome.com>
2023-11-22 07:44:04 -0300

    format code

--- internal/ibge/coordinates_test.go
+++ internal/ibge/coordinates_test.go
@@ -1,16 +1,12 @@
 package ibge
 
 import (
-	"fmt"
 	"testing"
-
 	"github.com/stretchr/testify/assert"
 )
 
 func TestGetCoordinatesIBGE(t *testing.T) {
 	coordinates, err := GetCoordinatesIBGE("4205407")
-
-	fmt.Println(coordinates)
 	assert.Nil(t, err)
 	assert.NotNil(t, coordinates)
 }
