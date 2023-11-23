b5193dc3ce9f6b60d98121bb4861de8a281f048f
davidsilva <david.silva@aiqfome.com>
2023-11-23 12:27:16 -0300

    refactor routes

---
+++ internal/api/route/route.go
@@ -0,0 +1,22 @@
+package route
+
+import (
+	"net/http"
+	"github.com/gorilla/mux"
+	"github.com/p4ndabk/ibge-integration/internal/api/controller"
+	"github.com/p4ndabk/ibge-integration/internal/api/middleware"
+)
+
+func InitRouter () (*mux.Router){
+	router := mux.NewRouter()
+
+	router.Handle("/api/health", middleware.SetContentTypeMiddleware(http.HandlerFunc(controller.HealthCheckHandlerRequest))).Methods("GET")
+	router.HandleFunc("/api/city/coordinates/{ibge_code}", controller.CheckCoordinateRequest).Methods("GET")
+
+	router.Handle("/api/city", middleware.SetContentTypeMiddleware(http.HandlerFunc(controller.AllCityRequest))).Methods("GET")
+	router.Handle("/api/city/{city_id}", middleware.SetContentTypeMiddleware(http.HandlerFunc(controller.CityRequest))).Methods("GET")
+
+	router.Handle("/api/solar-efficiencie/{city_id}", middleware.SetContentTypeMiddleware(http.HandlerFunc(controller.SolarEfficiencieByCodeRequest))).Methods("GET")
+
+	return router
+}
--- main.go
+++ main.go
@@ -2,23 +2,13 @@ package main
 
 import (
 	"fmt"
-	"github.com/gorilla/mux"
-	"github.com/p4ndabk/ibge-integration/internal/controller"
-	"github.com/p4ndabk/ibge-integration/internal/middleware"
+	"github.com/p4ndabk/ibge-integration/internal/api/route"
 	"log"
 	"net/http"
 )
 
 func main() {
-	router := mux.NewRouter()
-
-	router.Handle("/api/health", middleware.SetContentTypeMiddleware(http.HandlerFunc(controller.HealthCheckHandlerRequest))).Methods("GET")
-	router.HandleFunc("/api/city/coordinates/{ibge_code}", controller.CheckCoordinateRequest).Methods("GET")
-
-	router.Handle("/api/city", middleware.SetContentTypeMiddleware(http.HandlerFunc(controller.AllCityRequest))).Methods("GET")
-	router.Handle("/api/city/{city_id}", middleware.SetContentTypeMiddleware(http.HandlerFunc(controller.CityRequest))).Methods("GET")
-
-	router.Handle("/api/solar-efficiencie/{city_id}", middleware.SetContentTypeMiddleware(http.HandlerFunc(controller.SolarEfficiencieByCodeRequest))).Methods("GET")
+	router := route.InitRouter()
 
 	fmt.Println("Server is running on port 4001")
 	log.Fatal(http.ListenAndServe(":4001", router))
