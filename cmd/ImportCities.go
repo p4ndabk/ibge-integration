80a5f809661a419c91fcef0aa8878b7b70dc3c37
davidsilva <david.silva@aiqfome.com>
2023-11-30 19:13:37 -0300

    create function exec commands

--- cmd/ImportCities.go
+++ cmd/ImportCities.go
@@ -1,17 +1,21 @@
-package main
+package cmd
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
+	"os"
 
 	_ "github.com/mattn/go-sqlite3"
 	"github.com/p4ndabk/ibge-integration/app/ibge"
 	"github.com/p4ndabk/ibge-integration/infra/database"
 )
 
-func main() {
+func CreateCitiesTable() (bool, error){
 	db, err := database.InitDB()
 	if err != nil {
 		log.Fatal(err)
+		return false, err
 	}
 
 	createCitiesTable := `
@@ -27,26 +31,63 @@ func main() {
 	_, err = db.Exec(createCitiesTable)
 	if err != nil {
 		log.Printf("%q: %s\n", err, createCitiesTable)
-		return
+		return false, err
 	}
-	cities, err := ibge.AllCiteis()
+
+	return true, nil
+}
+
+func ImportCities() (bool, error){
+	db, err := database.InitDB()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return false, err
 	}
+
+	cities, err := importCitiesFile()
+	if err != nil {
+		log.Fatal(err)
+		return false, err		
+	}
+
 	tx, err := db.Begin()
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
+		return false, err
 	}
+
+	i := 0
 	for _, city := range cities.Cities {
+
 		stmt, err := tx.Prepare("insert into cities(code_ibge, code_uf, name, latitude, longitude, capital) values(?, ?, ?, ?, ?, ?)")
 		if err != nil {
-			panic(err)
-		}	
-		defer stmt.Close()
+			return false, err
+		}
 		_, err = stmt.Exec(city.CodeIBGE, city.CodeUF, city.Name, city.Latitude, city.Longitude, city.Capital)
 		if err != nil {
-			panic(err)
+			return false, err
 		}
+		i++
 	}
+
+	fmt.Println("Total of cities imported: ", i)
+	
 	tx.Commit()
+	return true, nil
+}
+
+func importCitiesFile() (ibge.Cities, error) {
+	var cities ibge.Cities
+
+	jsonData, err := os.Open("storage/cities.json")
+	if err != nil {
+		return cities, err		
+	}
+
+	decoder := json.NewDecoder(jsonData)
+	if err := decoder.Decode(&cities); err != nil {
+		return cities, err
+	}
+
+	return cities, nil
 }
